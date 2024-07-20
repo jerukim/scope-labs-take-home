ac0df00c23bb9ed16cbb7804c87dbd70089b4cf9
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-07-20 18:13:41 -0400

    add shadow to header and replace logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,15 @@ import { UploadButton } from './UploadButton'
 
 export function Header() {
   return (
-    <header className="h-12 flex px-2 justify-between items-center">
+    <header className="h-12 flex px-2 justify-between items-center md:h-16 md:px-6 lg:px-12 shadow">
       <Link href="/">
         <Image
-          className="size-11"
-          src="/LOGO_ICON.png"
+          className="h-11 w-auto"
+          src="/FULL_LOGO_COLOR.png"
           alt="Learnwell logo"
           priority
-          width={44}
-          height={44}
+          width={315}
+          height={87}
         />
       </Link>
 
